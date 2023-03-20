863ea652e6b56885bf91448ecf3250a6f2160810
Gyanu <gyanusherchan@outlook.com>
2023-03-20 16:42:33 +1100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.scss'
-import Home from './components/Home'
-import Layout from './components/Layout'
 
 function App() {
-  return (
-    <>
-      <Routes>
-        {/* here we gonna define the first path which going to hold the layout component */}
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>} />
-        </Route>
-      </Routes>
-    </>
-  )
+  return <div>lets start the portfolio</div>
 }
 export default App
